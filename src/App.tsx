f28d78469f5a5aebdc8796c5884313a59b619d87
mhimranhossain <mh.imranhossain@outlook.com>
2023-01-30 14:56:57 +0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Card } from "./lib/Card";
 import { Navbar } from "./lib/Navbar";
+import { useState } from "react";
 
 export const App = () => {
+  const [feedback, setFeedback] = useState("");
   return (
     <>
       <Navbar />
---
+++ src/data.tsx
@@ -0,0 +1,60 @@
+export const data = () => {
+  return (
+    <div>data</div>
+  );
+};
+
+/*
+[
+  {
+    "id": 1,
+    "rating": 5,
+    "text": "This is a great product!"
+  },
+  {
+    "id": 2,
+    "rating": 4,
+    "text": "I really like it!"
+  },
+  {
+    "id": 3,
+    "rating": 3,
+    "text": "It's ok."
+  },
+  {
+    "id": 4,
+    "rating": 2,
+    "text": "It's not great."
+  },
+  {
+    "id": 5,
+    "rating": 1,
+    "text": "It's terrible."
+  },
+  {
+    "id": 6,
+    "rating": 5,
+    "text": "It's amazing!"
+  },
+  {
+    "id": 7,
+    "rating": 4,
+    "text": "I really enjoy it."
+  },
+  {
+    "id": 8,
+    "rating": 3,
+    "text": "It's so-so."
+  },
+  {
+    "id": 9,
+    "rating": 2,
+    "text": "It's not great."
+  },
+  {
+    "id": 10,
+    "rating": 1,
+    "text": "It's horrible."
+  }
+]
+*/
