65a7b46648732ec89d0e4e59a19f794b6795b0b0
mhimranhossain <mh.imranhossain@outlook.com>
2023-01-30 21:11:49 +0600

    Update 01/30/2023 21:11:49

--- src/lib/Card.tsx
+++ src/lib/Card.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
+
+const example = "This is an example of a feedback item";
+
 export const Card = () => {
-  const [first, setfirst] = useState(5);
+  const [rating, setRating] = useState(7);
+  const [text, setText] = useState(example);
   return (
     <>
       <div className="bg-slate-300 py-10 text-center rounded-3xl m-5 relative">
         <div className="translate-x-1/2 -translate-y-1/2 absolute top-0 right-0 origin-center w-10 h-10 bg-blue-500 rounded-full border-4 border-blue-400 text-fuchsia-50 text-3xl font-mono font-black flex justify-center items-center">
-          {first}
+          {rating}
         </div>
         <p className="text-slate-900 text-xl font-medium">
-          This is an example of a feedback item
+          {text}
         </p>
       </div>
     </>
