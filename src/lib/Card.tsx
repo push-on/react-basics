78cb7f7d645e45bd718e9532bf3b1d0c45cf8bee
mhimranhossain <mh.imranhossain@outlook.com>
2023-01-31 09:26:40 +0600

    Update 01/31/2023 09:26:40

--- src/lib/Card.tsx
+++ src/lib/Card.tsx
@@ -1,6 +1,3 @@
-// import { useState } from "react";
-// const example = "This is an example of a feedback item";
-
 type fb = {
   fb: {
     id: number;
@@ -15,11 +12,11 @@ export const Card = ({ fb }: fb) => {
     <>
       {fb.map((user) => (
         <div className="bg-slate-300 py-10 text-center rounded-3xl m-10 relative">
-          <div className="w-16 h-16 translate-x-1/2 -translate-y-1/2 absolute top-0 right-0 origin-center  bg-blue-500 rounded-full border-2 text-fuchsia-50 text-3xl font-mono flex justify-center items-center">
+          <div className="w-10 h-10 -translate-x-1/2 -translate-y-1/2 absolute top-0 origin-center  bg-blue-500 rounded-full border-2 text-fuchsia-50 text-xl font-mono flex justify-center items-center">
             {user.id}
           </div>
           <p className="text-slate-900 text-xl font-medium">
-            "{user.text}"
+           Review: "{user.text}"
           </p>
           <p className="text-slate-900 text-xl font-medium">rating: {user.rating}</p>
         </div>
